9e7251393c4a3446f420531347370373a1d6020d
Brice Nelson <bnelsonemail@icloud.com>
2025-12-06 01:56:02 -0500

    chore: added pre commit config file

--- src/jea_meeting_web_scraper/NLP/NLP_test.py
+++ src/jea_meeting_web_scraper/NLP/NLP_test.py
@@ -3,7 +3,9 @@
 import spacy
 
 nlp = spacy.load("en_core_web_sm")
-doc = nlp("The JEA board approved a $2 million stormwater rehabilitation project for downtown Jacksonville.")
+doc = nlp(
+    "The JEA board approved a $2 million stormwater rehabilitation project for downtown Jacksonville."
+)
 
 for ent in doc.ents:
     print(ent.text, ent.label_)
